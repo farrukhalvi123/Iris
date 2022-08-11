218a240ae0aa553ef1a8a2b9eafe7e1b7ee4c34d
farrukhalvi123 <59420418+farrukhalvi123@users.noreply.github.com>
2022-08-11 13:26:24 -0700

    Add files via upload

---
+++ Constants/URLS.py
@@ -0,0 +1,6 @@
+class TestData:
+    BROWSER = "chrome"
+    draganddroppage = "https://demoqa.com/droppable"
+    practiceformpage = "https://demoqa.com/automation-practice-form"
+    frames_url = "https://demoqa.com/frames"
+    dynamicprop = "https://demoqa.com/dynamic-properties"
