758614b3759be56b2c340a017a1cba01c49b9288
Farrukh123 <PMXSEePs4hXRPrBm28DJ>
2022-08-13 07:59:33 -0700

    fixed error in viewpage

--- features/steps/config.py
+++ features/steps/config.py
@@ -11,20 +11,20 @@ import os
 from Pages.Login import login
 from Pages.view_page import view_homepage
 
+
 @given("Launch the browser")
 def step_impl(context):
     if TestData.BROWSER == 'chrome':
         options = webdriver.ChromeOptions()
-        # options.add_argument("no-sandbox")
-        # options.add_argument("--disable-gpu")
-        # options.add_argument("--window-size=800,600")
-        # options.add_argument("--ignore-certificate-error")
-        # options.add_argument("--ignore-ssl-errors")
-        # options.add_argument("--disable-notifications")
-        # # options.add_argument("download.default_directory=C:\\Users\\786 Computers\\PycharmProjects\\E2E-Web\\TestData\\Downloaded_Files")
-        # options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        # options.add_experimental_option('useAutomationExtension', False)
-        # options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=800,600")
+        options.add_argument("--ignore-certificate-error")
+        options.add_argument("--ignore-ssl-errors")
+        options.add_argument("--disable-notifications")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument("--disable-blink-features=AutomationControlled")
         context.driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
         context.driver.maximize_window()
     elif TestData.BROWSER == 'firefox':
@@ -38,12 +38,11 @@ def step_impl(context):
     elif TestData.BROWSER == 'edge':
         context.driver = webdriver.Edge(executable_path=EdgeChromiumDriverManager().install())
 
+
 @then(u'Close the browser')
 def step_impl(context):
     context.driver.close()
     context.driver.quit()
-
-
 @when("Open the iris URL")
 def step_impl(context):
     context.driver.get(TestData.iris_app_login_page)
