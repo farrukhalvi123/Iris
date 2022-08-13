469783b08497b72af2e152fdbb2ccbb6069f9aee
Farrukh123 <PMXSEePs4hXRPrBm28DJ>
2022-08-13 05:05:34 -0700

    fixed error in viewpage

--- Pages/view_page.py
+++ Pages/view_page.py
@@ -1,9 +1,10 @@
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class view_homepage:
-    def __int__(self, driver):
+    def __init__(self,driver):
         self.driver = driver
         self.drop_down_selector = "//div[@class='mat-select-panel-wrap ng-tns-c60-1 ng-trigger ng-trigger-transformPanelWrap ng-star-inserted']"
         self.search_field = "//app-root/div[@class='container-fluid']/app-asset-or-project[@class='ng-star-inserted']/div//app-search//input[@type='text']"
@@ -13,11 +14,11 @@ class view_homepage:
         self.expand_more_50 = "//span[normalize-space()='50']"
         self.expand_more_100 = "//span[normalize-space()='100']"
     def pagination_click_50(self):
-        self.find_element(By.XPATH, self.drop_down_selector ).click()
-        self.find_element(By.XPATH, self.expand_more_50).click()
+        self.driver.find_element(By.XPATH, self.drop_down_selector ).click()
+        self.driver.find_element(By.XPATH, self.expand_more_50).click()
 
     def pagination_click_100(self):
-        self.find_element(By.XPATH, self.drop_down_selector).click()
-        self.find_element(By.XPATH, self.expand_more_100).click()
+        self.driver.find_element(By.XPATH, self.drop_down_selector).click()
+        self.driver.find_element(By.XPATH, self.expand_more_100).click()
 
 
